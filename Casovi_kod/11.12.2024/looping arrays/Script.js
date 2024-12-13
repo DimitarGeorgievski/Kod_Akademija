7ead78438c2bb6c9529bb5bc8db03c4dbd671246
Dimitar Georgievski <muzipuzi123@gmail.com>
2024-12-13 20:45:18 +0100

    13.12.2024 chas

--- Casovi_kod/11.12.2024/looping arrays/Script.js
+++ Casovi_kod/11.12.2024/looping arrays/Script.js
@@ -45,3 +45,55 @@ function digitsInNumber(num){
     }
 }
 digitsInNumber(123456789);
+let counter =0;
+do {
+    // console.log(counter)
+    counter++;
+}
+while (counter < 10); //ke izbroi od 1-10
+let index = 0;
+while (index<10){
+    index++;
+}
+for (let i=0; i>10; i++){
+    // console.log(i)
+}
+let days = ["monday", "tuesday", "Wednesday", "thursday", "friday" , "saturday", "sunday"];
+for (let i = 0; i < days.length; i++){
+    let elements =days[i];
+    // console.log(elements);
+}
+for(let i = days.length - 1; i>= 10; i--){
+    let elements =days[i]
+    // console.log(elements)
+}
+let days1 = ["monday", "tuesday", "Wednesday", "thursday", "friday" , "saturday", "sunday"];
+for(let day of days1){
+    // console.log(day+ " for of")
+}
+for (let i = 0; i < 20; i++){
+    if(i >10){
+        break;
+    }
+}
+let index1 = 0;
+while(index1 < 30){
+    if(index1 % 3 === 0){
+        index1++;
+        continue;
+    }
+    // console.log(index);
+    index1++;
+}
+for(let index =0; index< 30; index++){
+    if (index% 3 === 0){
+        continue;
+    }
+    // console.log(index)
+}
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+for(let number of number){
+    if(number ===12){
+        break};
+        // console.log(number)
+}
---
+++ Casovi_kod/11.12.2024/vezbi/vezba15/script.js
@@ -0,0 +1,12 @@
+function proverkaNiza(item){
+    let niza1 = ["Dimitar","Marija","Petar"];
+    for(let i of niza1){
+        if(i === input){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+console.log(proverkaNiza());
