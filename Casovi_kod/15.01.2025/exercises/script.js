0e55ad98fd7a34c74df8c952d75c12ef74b8f30c
Dimitar Georgievski <muzipuzi123@gmail.com>
2025-01-17 17:46:12 +0100

    1.1 verzija vezba todo list

--- Casovi_kod/15.01.2025/exercises/script.js
+++ Casovi_kod/15.01.2025/exercises/script.js
@@ -1,57 +1,69 @@
-//variables
-let form = document.getElementById("add-todo-form");
-let todoInMemory = {
-    todo: [],
-    todoIdCounter: 1
-}
-
-//functions
-function Todo(id,title,description){
+// Construtor functions (models)
+function Todo(id, title, description) {
     this.id = id;
     this.title = title;
     this.description = description;
     this.isComplete = false;
+}
+
+// GLOBAL VARIABLES
+let todoInMemory = {
+    todos: [],
+    todoIdCounter: 1
 };
-function createTodo(title,description){
-    let todo = new Todo(todoInMemory.todoIdCounter,title,description);
+
+let form = document.getElementById('add-todo-form');
+
+// Functions
+function createTodo(title, description) {
+    let todo = new Todo(todoInMemory.todoIdCounter, title, description);
     todoInMemory.todoIdCounter += 1;
     return todo;
 }
-function resetInputs(parentElement){
-    let inputs = parentElement.getElementsByTagName("input");
-    for(let input of inputs){
-        input.value = "";
+
+function resetInputs(parentElement) {
+    let inputs = parentElement.getElementsByTagName('input');
+    for (let input of inputs) {
+        input.value = '';
     }
 }
-function getValuesFromInputs(parentElement){
-    let inputs = parentElement.getElementsByTagName("input");
+
+function getValuesFromInputs(parentElement) {
+    let inputs = parentElement.getElementsByTagName('input');
     let inputValues = {};
-    for(let input of inputs){
+    for (let input of inputs) {
         inputValues[input.name] = input.value;
     }
     return inputValues;
 }
-function showHideElement(element, isHiden){
-    // if(isHiden){
-    //     element.style.display = "none";
+
+function showHideElement(element, isHiden) {
+    // if(isHiden) {
+    //     element.style.display = 'none';
+    // } else {
+    //     element.style.display = 'block';
     // }
-    // else{
-    //     element.style.display = "block";
-    // } istoto e ova
-    element.style.display = isHiden ? "none" : "block";
+    element.style.display = isHiden ? 'none' : 'block';
 }
 
-document.querySelector("#add-todo").addEventListener("click", function(){
-    showHideElement(form,true);
-});
-document.getElementById("reset-todo").addEventListener("click", function(){
-    resetInputs(form);
-});
-document.getElementById("save-todo").addEventListener("click", function(){
-    let inputValues = getValuesFromInputs(form);
-    let todo = createTodo(inputValues.title,inputValues.description);
-    todoInMemory.todo.push(todo);
-    resetInputs(form);
-    console.log(todo);
-    showHideElement(form,true);
-});
+// EVENTS
+document.querySelector('#add-todo')
+    .addEventListener('click', function () {
+        showHideElement(form, false);
+    });
+
+document.getElementById('reset-todo')
+    .addEventListener('click', function () {
+        resetInputs(form);
+    });
+
+document.getElementById('save-todo')
+    .addEventListener('click', function () {
+        let inputValues = getValuesFromInputs(form);
+        let todo = createTodo(inputValues.title, inputValues.description);
+        todoInMemory.todos.push(todo);
+        resetInputs(form);
+        form.style.display = 'none';
+        console.log(todoInMemory.todos);
+        showHideElement(form, true);
+    });
