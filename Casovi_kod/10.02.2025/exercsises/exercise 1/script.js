ee46be5bbf30ac93ce826aff80401b799fe455e4
DimitarGeorgievski <muzipuzi123@gmail.com>
2025-02-13 23:37:10 +0100

    1.2v Zadaci finito

--- Casovi_kod/10.02.2025/exercsises/exercise 1/script.js
+++ Casovi_kod/10.02.2025/exercsises/exercise 1/script.js
@@ -1,43 +1,83 @@
-function Academy(name,student,subjects){
+function Academy(name, students, subjects) {
     this.name = name;
-    this.students = [new AcademySubjects(student)];
-    this.subjects = [new AcademySubjects(subjects)];
+    this.subjects = subjects.map(x => new AcademySubjects(x));
+    this.students = students.map(x => new AcademyStudents(x));
     this.start = new Date();
     this.end = new Date();
     this.numberOfClasses = subjects.length * 10;
     this.printStudents = function(){
-        this.students.forEach(student => {
-            console.log(student);
-        });
-    }
+        this.students.forEach(student => console.log(student));
+    };
     this.printSubjects = function(){
-        this.subjects.forEach(subjects => {
-            console.log(subjects);
-        })
-    }
-    }
+        this.subjects.forEach(subject => console.log(subject));
+    };
+}
+
 function AcademySubjects(subject){
-    this.subject = subject
+    this.subject = subject;
 }
+
 function AcademyStudents(student){
-    this.subject = student;
+    this.student = student;
 }
-let Qinshift = new Academy("Qinshift",["Petar","Trajko"],["C#","node.js"]);
+
+let Qinshift = new Academy("Qinshift",["Petar", "Trajko"],["math", "english"]);
 console.log(Qinshift);
 Qinshift.printSubjects();
 Qinshift.printStudents();
+
 // -------------------------------------------------------------
 
-function Students(title,numberOfClasses = 10,isElective,academy,students){
+function Subject(title,isElective,academy,students) {
     this.title = title;
-    this.numberOfClasses = numberOfClasses;
+    this.numberOfClasses = 10;
     this.isElective = isElective;
     this.academy = academy;
     this.students = students;
+    this.overrideClasses = function(num){
+        if(num >= 3){
+            this.numberOfClasses = num;
+        }
+        console.log(this.numberOfClasses);
+    };
+}
+let predmet = new Subject("Math",true,"Qinshift",["Trajko", "Petko"]);
+console.log(predmet);
+predmet.overrideClasses(5);
+
+// -------------------------------------------------------------
+
+function Students(firstName,lastName,age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.completedSubjects = [];
+    this.academy = null;
+    this.currentSubject = null;
+    this.startAcademy = function(academy){
+        this.academy = academy;
+        this.academy.students.push(`${this.firstName} ${this.lastName}`)
+    };
+    this.startSubject = function(subjectObject){
+        if(this.academy){
+            for(let subject of this.academy.subjects){
+                if(subject.subject.toLowerCase() === subjectObject.title.toLowerCase()){
+                    if(this.currentSubject){
+                        this.completedSubjects.push(this.currentSubject);
+                    }
+                        this.currentSubject = subjectObject.title;
+                        subjectObject.students.push(`${this.firstName} ${this.lastName}`);
+                    console.log(`You have successfully added ${this.currentSubject} to your schedule`);
+                }
+            }
+        }
+    if(!this.currentSubject){
+        console.log("error");
+    }
+    };
 }
 
-// ---------------------------------------------------------------
-
-function Subject(){
-
-}
+let student1 = new Students("Petar", "Trajkovski", 19);
+let math = new Subject("Math", true, "Qinshift", ["Trajko", "Petko"]);
+student1.startAcademy(Qinshift);
+student1.startSubject(math);
