22701b5fb7f6e7422efbc53e4e161d07cbb4d990
DimitarGeorgievski <muzipuzi123@gmail.com>
2025-05-13 18:46:09 +0200

    changed some stuff

--- Casovi_kod/advanced NestJS/09.05.2025/products-api/src/orders/entities/order.entity.ts
+++ Casovi_kod/advanced NestJS/09.05.2025/products-api/src/orders/entities/order.entity.ts
@@ -12,7 +12,7 @@ export class Order {
     date: Date;
     @ManyToOne(() => User, user => user.orders)
     user: User;
-    @ManyToMany(() => Product)
+    @ManyToMany(() => Product, product => product.orders)
     @JoinTable()
     products: Product[] // ne mi bese jasno kako raboti @RelationId i dali treba da go koristam, pa ako mozi da se objasni na chas ili vo komentar
 }
