5875b8c799428f5e39e440b435345e0918e449b9
DimitarGeorgievski <muzipuzi123@gmail.com>
2025-06-15 00:53:41 +0200

    d

--- Casovi_kod/advanced NestJS/02.06.2025/movies-api/src/movies/movies.service.ts
+++ Casovi_kod/advanced NestJS/02.06.2025/movies-api/src/movies/movies.service.ts
@@ -8,6 +8,7 @@ import { Movie } from './entities/movie.entity';
 @Injectable()
 export class MoviesService {
   constructor(@InjectRepository(Movie) private moviesRepo: Repository<Movie>) {}
+
   create(createMovieDto: CreateMovieDto) {
     return 'This action adds a new movie';
   }
@@ -15,9 +16,10 @@ export class MoviesService {
   findAll() {
     const queryBuilder = this.moviesRepo
       .createQueryBuilder('movies')
-      .select(['movies.id', 'movies.title', 'movies.budget']);
-    queryBuilder.offset(5);
-    queryBuilder.limit(5);
+      .select(['movies.id', 'movies.title', 'movies.budget'])
+      .offset(5)
+      .limit(5);
+
     return queryBuilder.getMany();
   }
 
@@ -28,32 +30,47 @@ export class MoviesService {
         director: true,
         genres: true,
         castMember: {
-          actors: true
-        }
+          actors: true,
+        },
       },
     });
-    const builderQuery = this.moviesRepo
-      .createQueryBuilder('movie')
-      .leftJoinAndSelect('movie.genres', 'genres')
-      .leftJoinAndSelect('movie.director', 'director')
-      .where('movie.id = :movieId', { movieId: id })
-      .getOne();
-    if (!foundMovie) throw new NotFoundException('movie not found');
+
+    // const builderQuery = this.moviesRepo
+    //   .createQueryBuilder('movie')
+    //   .leftJoinAndSelect('movie.genres', 'genre')
+    //   .leftJoinAndSelect('movie.director', 'director')
+    //   .where('movie.id = :movieId', { movieId: id })
+    //   .getOne();
+
+    // const rawQuery = this.moviesRepo.query(
+    //   `
+    //     select * from movies m
+    //     left join movie_genres mg on mg.movie_id = m.movie_id 
+    //     left join genres g on mg.genre_id  = g.genre_id 
+    //     left join directors d on d.director_id = m.director_id 
+    //     where m.movie_id = $1;
+    //   `,
+    //   [id],
+    // );
+    if (!foundMovie) throw new NotFoundException('Movie not found');
+
     return foundMovie;
   }
+
   async getMovieCountByGenre() {
     const response = await this.moviesRepo
       .createQueryBuilder('movie')
-      .leftJoin('movie.genres', 'genres')
-      .select("genres.id", "id")
-      .addSelect("genres.name", "name")
-      .addSelect("COUNT(movie.id)", "movieCount")
-      .groupBy("genres.id")
-      .orderBy("genres.id")
+      .leftJoin('movie.genres', 'genre')
+      .select('genre.id', 'id')
+      .addSelect('genre.name', 'name')
+      .addSelect('COUNT(movie.id)', 'movieCount')
+      .groupBy('genre.id')
+      .orderBy('genre.id')
       .getRawMany();
 
-    return response.map((r) => ({...r, movieCount: Number(r.movieCount)}));
+    return response.map((r) => ({ ...r, movieCount: Number(r.movieCount) }));
   }
+
   update(id: number, updateMovieDto: UpdateMovieDto) {
     return `This action updates a #${id} movie`;
   }
