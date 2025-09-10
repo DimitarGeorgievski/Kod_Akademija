addf594e72ba5a31f6ed02059d02185c819b1a65
DimitarGeorgievski <muzipuzi123@gmail.com>
2025-09-10 20:33:21 +0200

    chas12

---
+++ Casovi_kod/Angular/chas12/movies-app/src/app/feature/movies/components/movie-details/movie-details.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { MoviesService } from '../../../../core/services/movies-service';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-movie-details',
+  imports: [ButtonComponent],
+  templateUrl: './movie-details.html',
+  styleUrl: './movie-details.scss',
+})
+export class MovieDetails {
+  private moviesService = inject(MoviesService);
+
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+
+  ngOnInit() {
+    console.log(this.route.snapshot);
+
+    const movieId: string = this.route.snapshot.params['id'];
+
+    this.moviesService.geMovieById(movieId);
+  }
+
+  selectedMovie = this.moviesService.selectedMovie;
+
+  onClickLikeDislike(type: 'LIKE' | 'DISLIKE') {
+    this.moviesService.addLikeDislike(type);
+  }
+
+  goToEditMovie() {
+    this.router.navigate(['edit-movie', this.selectedMovie().id]);
+  }
+}
